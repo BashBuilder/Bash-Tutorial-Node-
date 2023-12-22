1fd26eddcde508e0bcf59daa6757986f61ff822d
BashBuilder <iamadelowotan@gmail.com>
2023-12-22 12:36:05 +0100

    vercel json addition commit

--- server.js
+++ server.js
@@ -4,16 +4,21 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 // the various paths inports
-const userAuthRoute = require('./Route/userAuthRoute');
-
+const userAuthRoute = require("./Route/userAuthRoute");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {app.listen(process.env.PORT), console.log("App is listerning")})
-  .catch(error => console.error(error))
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT), console.log("App is listerning");
+  })
+  .catch((error) => console.error(error));
 
-app.use('/api/school', userAuthRoute);
+app.use("/api/school", userAuthRoute);
 
+app.get("/", (req, res) => {
+  res.status(200).json({ home: "Hello wolcome" });
+});
